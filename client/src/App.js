e6c1edecefbe6fde2c42652064824dc38e1c31a6
Rob Birch <rbirchtree@gmail.com>
2020-03-27 15:30:33 -0700

    updated dns again

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   componentDidMount = () => {
     // Make sure to change the (localhost) on the line bellow
     // to the public DNS of your EC2 instance
-    axios.get(`ec2-13-56-248-182.us-west-1.compute.amazonaws.com`).then(res => {
+    axios.get(`ec2-54-241-88-147.us-west-1.compute.amazonaws.com`).then(res => {
       const dataFromServer = res.data;
       this.setState({ data: dataFromServer });
     });
