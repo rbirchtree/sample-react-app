f8895a967dd4ee0c97941755a58ff69661441a3d
Rob Birch <rbirchtree@gmail.com>
2020-03-27 15:05:11 -0700

    updated dns

--- client/src/App.js
+++ client/src/App.js
@@ -6,26 +6,22 @@ import './App.css';
 class App extends Component {
   state = {
     data: ''
-  }
+  };
   componentDidMount = () => {
-    // Make sure to change the (localhost) on the line bellow 
+    // Make sure to change the (localhost) on the line bellow
     // to the public DNS of your EC2 instance
-    axios.get(`http://localhost:4000/sayHello`)
-    .then(res => {
+    axios.get(`ec2-13-56-248-182.us-west-1.compute.amazonaws.com`).then(res => {
       const dataFromServer = res.data;
       this.setState({ data: dataFromServer });
     });
-  }
+  };
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>Server is saying:</p>
-          <p>
-            {this.state.data}
-          </p>
-          
+          <p>{this.state.data}</p>
         </header>
       </div>
     );
